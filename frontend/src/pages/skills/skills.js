214eaa6244646a7edba6fb5cb6fa536491ed4111
Dieuveille MPOUNKOUO <94381407+DieuveilleSuthi@users.noreply.github.com>
2024-05-24 02:11:22 +0200

    skills

--- frontend/src/pages/skills/skills.js
+++ frontend/src/pages/skills/skills.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import { skillsUtils } from "../../utils/skillsUtils";
+import './skills.css';
 
 const Skills = () => {
+    const [selectedCategory, setSelectedCategory] = useState('All');
+
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const filteredSkills = selectedCategory === 'All'
+        ? skillsUtils
+        : skillsUtils.filter(skill => skill.category === selectedCategory);
+
     return (
         <div>
-            <p> skills </p>
+            <h1 className="title">Skills</h1>
+            <div className="buttonsContainer">
+                <ul>
+                    <li className={`btn ${selectedCategory === 'All' ? 'active' : ''}`} onClick={() => handleCategoryClick('All')}>All</li>
+                    <li className={`btn ${selectedCategory === 'Language' ? 'active' : ''}`} onClick={() => handleCategoryClick('Language')}>Programming Languages</li>
+                    <li className={`btn ${selectedCategory === 'Frameworks' ? 'active' : ''}`} onClick={() => handleCategoryClick('Frameworks')}>Frameworks</li>
+                    <li className={`btn ${selectedCategory === 'Other' ? 'active' : ''}`} onClick={() => handleCategoryClick('Other')}>Tools/Others</li>
+                </ul>
+            </div>
+            <div className="skillsList">
+                <ul>
+                    {filteredSkills.map(skill => (
+                        <li key={skill.id}>{skill.name}</li>
+                    ))}
+                </ul>
+            </div>
+
         </div>
-    )
+    );
 }
 
 export default Skills;
---
+++ frontend/src/utils/skillsUtils.js
@@ -0,0 +1,97 @@
+export const skillsUtils = [
+    {
+        id: 1,
+        name: 'JavaScript',
+        category: 'Language'
+    },
+    {
+        id: 2,
+        name: 'Java',
+        category: 'Language'
+    },
+    {
+        id: 3,
+        name: 'Python',
+        category: 'Language'
+    },
+    {
+        id: 4,
+        name: 'C',
+        category: 'Language'
+    },
+    {
+        id: 5,
+        name: 'C#',
+        category: 'Language'
+    },
+    {
+        id: 6,
+        name: 'NodeJS',
+        category: 'Language'
+    },
+    {
+        id: 7,
+        name: 'Html/CSS',
+        category: 'Language'
+    },
+    {
+        id: 8,
+        name: 'ReactJS',
+        category: 'Frameworks'
+    },
+    {
+        id: 9,
+        name: 'React-Native',
+        category: 'Frameworks'
+    },
+    {
+        id: 10,
+        name: 'VueJS',
+        category: 'Frameworks'
+    },
+    {
+        id: 11,
+        name: 'NextJS',
+        category: 'Frameworks'
+    },
+    {
+        id: 12,
+        name: 'ExpressJS',
+        category: 'Frameworks'
+    },
+    {
+        id: 13,
+        name: 'SQL',
+        category: 'Other'
+    },
+    {
+        id: 14,
+        name: 'MySQL',
+        category: 'Other'
+    },
+    {
+        id: 15,
+        name: 'MongoDB',
+        category: 'Other'
+    },
+    {
+        id: 16,
+        name: 'Oracle',
+        category: 'Other'
+    },
+    {
+        id: 17,
+        name: 'Linux',
+        category: 'Other'
+    },
+    {
+        id: 18,
+        name: 'Git',
+        category: 'Other'
+    },
+    {
+        id: 19,
+        name: 'Docker',
+        category: 'Other'
+    }
+]
