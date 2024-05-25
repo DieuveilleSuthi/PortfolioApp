b1f8ddc75bd095686b0ef01ceddba6916eda5f4d
Dieuveille MPOUNKOUO <94381407+DieuveilleSuthi@users.noreply.github.com>
2024-05-25 02:23:25 +0200

    Create projectsUtils.js

---
+++ frontend/src/utils/projectsUtils.js
@@ -0,0 +1,40 @@
+
+
+export const skillsUtils = [
+    {
+        id: 1,
+        name: 'PlanningApp',
+        image: javascriptLogo,
+        category: 'mobile'
+    },
+    {
+        id: 2,
+        name: 'GuessingApp',
+        image: javaLogo,
+        category: 'web'
+    },
+    {
+        id: 3,
+        name: 'BanqueApp',
+        image: pythonLogo,
+        category: 'web'
+    },
+    {
+        id: 4,
+        name: 'Shoppify',
+        image: cLogo,
+        category: 'web'
+    },
+    {
+        id: 5,
+        name: 'MyEshop',
+        image: csharpLogo,
+        category: 'web'
+    },
+    {
+        id: 6,
+        name: 'RoboFriends',
+        image: nodeLogo,
+        category: 'web'
+    }
+]
