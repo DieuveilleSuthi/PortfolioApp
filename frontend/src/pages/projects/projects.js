0aa0c09879a21f8f763c126daacf71138c5c62d2
Dieuveille MPOUNKOUO <94381407+DieuveilleSuthi@users.noreply.github.com>
2024-05-25 15:52:33 +0200

    projects

--- frontend/src/pages/projects/projects.js
+++ frontend/src/pages/projects/projects.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { projectsUtils } from "../../utils/projectsUtils";
+import './projects.css';
 
 const Projects = () => {
+    const [selectedCategory, setSelectedCategory] = useState('All');
+
+    const handleCategoryClick = (category) => {
+        setSelectedCategory(category);
+    };
+
+    const filteredProjects = selectedCategory === 'All'
+        ? projectsUtils
+        : projectsUtils.filter(project => project.category === selectedCategory);
+
     return (
         <div>
-            <p> projects </p>
+            <h1 className="title">My recents Projects</h1>
+            <div className="buttonsContainer">
+                <ul>
+                    <li className={`btn ${selectedCategory === 'All' ? 'active' : ''}`} onClick={() => handleCategoryClick('All')}>All</li>
+                    <li className={`btn ${selectedCategory === 'mobile' ? 'active' : ''}`} onClick={() => handleCategoryClick('mobile')}>Mobile</li>
+                    <li className={`btn ${selectedCategory === 'web' ? 'active' : ''}`} onClick={() => handleCategoryClick('web')}>Web</li>
+                </ul>
+            </div>
+            <div className="projectsList">
+                {filteredProjects.map(project => (
+                    <div key={project.id} className="projectItem">
+                        <img src={project.image} alt="notImg" className="logo" />
+                        <div>
+                            <h3>{project.name}</h3>
+                            <h4>{project.description}</h4>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    )
+    );
 }
 
 export default Projects;
--- frontend/src/utils/projectsUtils.js
+++ frontend/src/utils/projectsUtils.js
@@ -1,40 +1,51 @@
+import BanqueApp from '../images/projectsImage/BanqueApp.png';
+import guessingGame from '../images/projectsImage/guessingGame.png';
+import MyEshop from '../images/projectsImage/MyEshop.png';
+import planningApp from '../images/projectsImage/planningApp.png';
+import Robofriends from '../images/projectsImage/Robofriends.png';
+import zando from '../images/projectsImage/zando.png';
 
-
-export const skillsUtils = [
+export const projectsUtils = [
     {
         id: 1,
         name: 'PlanningApp',
-        image: javascriptLogo,
+        description: 'Planning App is the ultimate planning solution created  designed to streamline your daily, weekly, and monthly schedules with effortless efficiency.',
+        image: planningApp,
         category: 'mobile'
     },
     {
         id: 2,
-        name: 'GuessingApp',
-        image: javaLogo,
+        name: 'GuessingGame',
+        description: 'Guessing game is a web game created in javaScript and html for guessing numbers',
+        image: guessingGame,
         category: 'web'
     },
     {
         id: 3,
         name: 'BanqueApp',
-        image: pythonLogo,
+        description:'Banque app is a clone of the Bankist app application created by Jonas Schmedtmann coded in html css and javascript',
+        image: BanqueApp,
         category: 'web'
     },
     {
         id: 4,
-        name: 'Shoppify',
-        image: cLogo,
+        name: 'Zando',
+        description: 'ZANDO is a sales site clothes, bags and many other things online coded in reactJS',
+        image: zando,
         category: 'web'
     },
     {
         id: 5,
         name: 'MyEshop',
-        image: csharpLogo,
+        description: 'MyEshop is an e-shop site allowing individuals to make their purchases online on other sites such as Amazon and many others. It is coded in html css and javascript',
+        image: MyEshop,
         category: 'web'
     },
     {
         id: 6,
         name: 'RoboFriends',
-        image: nodeLogo,
+        description: 'Robofriends is a small web application created in react to display and filter components in card form',
+        image: Robofriends,
         category: 'web'
     }
 ]
